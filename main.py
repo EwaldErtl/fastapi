13a0a223adb57c74c070d6306f1566c94e5b7e5f
Ewald Ertl <ewald.ertl@axxiome.com>
2021-12-23 13:08:41 +0000

    Adding Path and query parameters with default values

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+from typing import Optional
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data':
-            {
-                'name':  'Ewald'
-            }}
+@app.get('/blog')
+def index(limit:int=10, published:bool=False, sort: Optional[str] = None):
+    if published:
+        return {'data': f'published blog list {limit}' }
+    else:
+        return {'data': f'blog list {limit}' }
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'unpublished'}
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about page' }
+@app.get('/blog/{id}')
+def about(id: int):
+    return {'data': id }
+
+@app.get('/blog/{id}/comments')
+def comments(id: int): 
+    return {'data': {'comments': {'1' :  'comment'}}}
